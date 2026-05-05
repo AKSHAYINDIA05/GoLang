fdea7e5f83763de0a7fbc7cb84803dba86bb0c5f
AKSHAY M <akshaym0507@outlook.com>
2026-05-05 07:09:36 +0530

    Functions Assignment

--- Functions/main.go
+++ Functions/main.go
@@ -1,12 +1,38 @@
+//package main
+
+//import "fmt"
+
+//func add(x int, y int) int {
+//	return x + y
+//}
+//
+//func main() {
+//	result := add(2, 4)
+//	fmt.Printf("Result : %v\n", result)
+//}
+
+//Assignment
+//We often will need to manipulate strings in our messaging app.
+//For example, adding some personalization by using a customer's name within a template.
+//The concat function should take two strings and smash them together.
+//hello + world = helloworld
+
 package main
 
 import "fmt"
 
-func add(x int, y int) int {
-	return x + y
+func concat(s1 string, s2 string) string {
+	return s1 + s2
 }
 
+// don't touch below this line
+
 func main() {
-	result := add(2, 4)
-	fmt.Printf("Result : %v\n", result)
+	test("Lane,", " happy birthday!")
+	test("Zuck,", " hope that Metaverse thing works out")
+	test("Go", " is fantastic")
+}
+
+func test(s1 string, s2 string) {
+	fmt.Println(concat(s1, s2))
 }
